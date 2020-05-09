fd1a7c792b63aaf36f67bda276ecab9cddad7b52
Matis Duperray <m.duperray@rubika-edu.com>
2020-05-09 20:21:14 +0200

    Best jeu ever

---
+++ ActionAventureGame/Assets/Scripts/WaitForAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class WaitForAudio : MonoBehaviour
+    {
+        public float time;
+
+
+        private void Start()
+        {
+            AudioSource source = GetComponent<AudioSource>();
+            StartCoroutine(WaitForPlay(source, time));
+        }
+
+
+        IEnumerator WaitForPlay(AudioSource _source, float _time)
+        {
+            yield return new WaitForSeconds(_time);
+            _source.enabled = true;
+        }
+    }
+}
