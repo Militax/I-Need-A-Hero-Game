7a5e08b7e63bbb16d87480f1ccdf82250d1194ec
Louis Lefebvre <l.lefebvre@rubika-edu.com>
2020-05-28 16:12:50 +0200

    Barrière implémentée

---
+++ ActionAventureGame/Assets/DestroyBossProjectils.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBossProjectils : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.tag.Equals("LightBullet")) { return; }
+        if(transform.position.y>collision.transform.position.y)GameObject.Destroy(collision.gameObject);
+    }
+}
