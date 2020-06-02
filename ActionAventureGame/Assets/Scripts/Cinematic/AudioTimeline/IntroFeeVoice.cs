d229a68202558f05d613074a031de9c071e9224a
Matis Duperray <m.duperray@rubika-edu.com>
2020-06-02 13:56:51 +0200

    things

--- ActionAventureGame/Assets/Scripts/Cinematic/AudioTimeline/IntroFeeVoice.cs
+++ ActionAventureGame/Assets/Scripts/Cinematic/AudioTimeline/IntroFeeVoice.cs
@@ -2,34 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Game
+public class IntroFeeVoice : MonoBehaviour
 {
-    public class IntroFeeVoice : MonoBehaviour
+    public AudioClip voiceLine9;
+
+
+    public void Play9()
     {
-        public AudioClip voiceLine9;
-        public AudioClip voiceLine10;
-        public AudioClip voiceLine11;
-        public AudioClip voiceLine12;
-
-
-        public void Play9()
-        {
-            SoundManager.instance.PlayVoices(voiceLine9, 1);
-        }
-        public void Play10()
-        {
-            SoundManager.instance.PlayVoices(voiceLine10, 1);
-        }
-        public void Play11()
-        {
-            SoundManager.instance.PlayVoices(voiceLine11, 1);
-        }
-        public void Play12()
-        {
-            SoundManager.instance.PlayVoices(voiceLine12, 1);
-        }
-
-
-
+        SoundManager.instance.PlayVoices(voiceLine9, 1);
     }
+
+
+
 }
