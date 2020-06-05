49661b445f570d10ac34ad9d04c16ff7d03f973c
Matis Duperray <m.duperray@rubika-edu.com>
2020-06-05 19:26:32 +0200

    Fin des sons urgents

---
+++ ActionAventureGame/Assets/Scripts/Events/AudioTimeline/IntroBossVoice.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroBossVoice : MonoBehaviour
+{
+    public AudioClip voiceLine34;
+    public AudioClip voiceLine35;
+    public AudioClip voiceLine36;
+    public AudioClip voiceLine37;
+    public AudioClip voiceLine38;
+
+
+    public void Play34()
+    {
+        SoundManager.instance.PlayVoices(voiceLine34, 1);
+    }
+    public void Play35()
+    {
+        SoundManager.instance.PlayVoices(voiceLine35, 1);
+    }
+    public void Play36()
+    {
+        SoundManager.instance.PlayVoices(voiceLine36, 1);
+    }
+    public void Play37()
+    {
+        SoundManager.instance.PlayVoices(voiceLine37, 1);
+    }
+    public void Play38()
+    {
+        SoundManager.instance.PlayVoices(voiceLine38, 1);
+    }
+
+
+
+}
