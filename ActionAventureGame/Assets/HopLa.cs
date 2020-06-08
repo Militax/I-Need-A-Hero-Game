ad4e6bdf7313bf83263c0430d057e1f380a43b39
Matis Duperray <m.duperray@rubika-edu.com>
2020-06-08 20:51:38 +0200

    hop la

---
+++ ActionAventureGame/Assets/HopLa.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameManagement;
+
+public class HopLa : MonoBehaviour
+{
+    public AudioClip NarratorVoice68;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "PlayerFeet" && GameManager.Instance.powerState >= 3)
+        {
+            SoundManager.instance.PlayVoices(NarratorVoice68, 1);
+            Destroy(gameObject);
+        }
+    }
+
+}
