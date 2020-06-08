932b29a52545ffa81e0abcc8af29fed2401969b8
Matis Duperray <m.duperray@rubika-edu.com>
2020-06-08 13:20:20 +0200

    Commit2

---
+++ ActionAventureGame/Assets/Scripts/Events/AudioTimeline/CaptureFeeForceVoice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureFeeForceVoice : MonoBehaviour
+{
+    public AudioClip voiceLine65;
+    public AudioClip voiceLine66;
+
+
+    public void Play65()
+    {
+        SoundManager.instance.PlayVoices(voiceLine65, 1);
+    }
+    public void Play66()
+    {
+        SoundManager.instance.PlayVoices(voiceLine66, 1);
+    }
+}
---
+++ ActionAventureGame/Assets/Scripts/Events/PlaySFX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFX : MonoBehaviour
+{
+    public bool onStart;
+    public bool ifTrigger;
+
+    [Range(0f, 1f)] public float volume;
+
+    public AudioClip SFX;
+
+
+    private void Start()
+    {
+        if (onStart)
+        {
+            SoundManager.instance.PlaySfx(SFX, volume, 1);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (ifTrigger && other.tag == "PlayerFeet")
+        {
+            SoundManager.instance.PlaySfx(SFX, volume, 1);
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void PlaySong()
+    {
+        SoundManager.instance.PlaySfx(SFX, volume, 1);
+    }
+}
