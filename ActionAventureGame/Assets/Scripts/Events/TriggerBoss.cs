382c9deb2bb532b9f8c528952ea0f7434be6a533
Aubane <a.rittano@rubika-edu.com>
2020-06-13 12:37:13 +0200

    ui disparait pendant events

--- ActionAventureGame/Assets/Scripts/Events/TriggerBoss.cs
+++ ActionAventureGame/Assets/Scripts/Events/TriggerBoss.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.Playables;
 using GameManagement;
 using Cinemachine;
-using UnityEngine.Playables;
 
 public class TriggerBoss : MonoBehaviour
 {
@@ -13,7 +12,7 @@ public class TriggerBoss : MonoBehaviour
 
     private bool TimelinePlayed;
     private int powerlvl;
-
+    public PlayableAsset playableAsset;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +22,30 @@ public class TriggerBoss : MonoBehaviour
         TimelinePlayed = false;
         Camera.main.GetComponent<BossCamera>().enabled = false;
         Camera.main.GetComponent<CinemachineBrain>().enabled = true;
-    }
+
+        
+
+    var outputs = playableAsset.outputs;
+        foreach(var itm in outputs)
+        {
+            if (itm.streamName == "PlayerLife")
+            {
+                Timeline.SetGenericBinding(itm.sourceObject, GameManager.Instance.GetComponent<CanvasManagement>().playerHealth);
+            }
+            if (itm.streamName == "Bourse")
+            {
+                Timeline.SetGenericBinding(itm.sourceObject, GameManager.Instance.GetComponent<CanvasManagement>().bourse);
+            }
+            if (itm.streamName == "Score")
+            {
+                Timeline.SetGenericBinding(itm.sourceObject, GameManager.Instance.GetComponent<CanvasManagement>().score);
+            }
+            if (itm.streamName == "Input")
+            {
+                Timeline.SetGenericBinding(itm.sourceObject, GameManager.Instance.GetComponent<CanvasManagement>().Input);
+            }
+        }
+}
 
     // Update is called once per frame
     void Update()
